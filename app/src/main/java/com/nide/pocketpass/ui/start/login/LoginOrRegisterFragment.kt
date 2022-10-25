c69608b834c7f3452c00896fef154e32251102e0
Debashis <debahalder1@gmail.com>
2022-10-25 18:16:41 +0530

    release v-4

--- app/src/main/java/com/nide/pocketpass/ui/start/login/LoginOrRegisterFragment.kt
+++ app/src/main/java/com/nide/pocketpass/ui/start/login/LoginOrRegisterFragment.kt
@@ -1,6 +1,7 @@
 package com.nide.pocketpass.ui.start.login
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -100,6 +101,7 @@ class LoginOrRegisterFragment : Fragment() {
     }
 
     private fun initView() = binding.apply {
+        inRegister.tvTermCondition.movementMethod = LinkMovementMethod.getInstance()
         loginAndRegisterTab.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
                 when (tab?.position) {
